0a8eb750c9b002a57c60b54d42b698769e9b2c1d
tbeiko <t.beiko23@gmail.com>
2016-01-09 17:50:12 -0500

    Change email/image for users

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,10 +4,7 @@ class User <ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.handle = "@" + auth["info"]["nickname"]
-      user.email = nil # we'll need to remove this
-      
-      # 'image' not included in users yet
-      user.email = auth["extra"]["raw_info"]["profile_image_url"] # This is just for testing
+      user.image = auth["extra"]["raw_info"]["profile_image_url"] 
       user.uid = auth["uid"]
       user.token = auth["credentials"]["token"] # We might not even need this
       user.provider = auth["provider"]
