f5feb21489edb46e30d9f694570b17ff7f13bdb5
tbeiko <t.beiko23@gmail.com>
2016-01-10 10:36:47 -0500

    remove tweet_index logic from tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,29 +17,6 @@ class TweetsController < ApplicationController
     end
 	end
 
-  def bot
-  end
-
-  def tweetout
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_API_KEY"] 
-      config.consumer_secret     = ENV["TWITTER_API_SECRET"] 
-      config.access_token        = '4757590114-Kok6YXQBEENVeCKJv3WScIaOjNgS599fHPDoCoS'
-      config.access_token_secret = 'gi8mWVzBJWCkElZm7vwR6423iXbwID2rLDmRklPQupgC0'
-    end
-
-    if current_user.has_reached_max_tweets?
-      current_user.reset_tweets_index
-    end
-
-    handle = current_user.handle
-    tweet = current_user.books.first.tweets.find_by(id: current_user.tweets_index + 1).body
-    current_user.increase_tweets_index
-    current_user.save!
-    @client.update(handle + " " + tweet)
-    redirect_to bot_path
-  end
-
 	private
 	def tweet_params
   	params.require(:tweet).permit(:body, :book_id)
