fe81f74957073513623e08f4fb80bbcf78f5649d
tbeiko <t.beiko23@gmail.com>
2016-01-10 11:24:30 -0500

    Add tweetout action to jobs

--- app/jobs/scheduleTweet.rb
+++ app/jobs/scheduleTweet.rb
@@ -1,6 +1,26 @@
 module ScheduleTweet
     @queue = :tweets
     def self.perform()
-        puts "Tweeted"
-    end
+      User.all.each do |user|
+        if ((user.books.count != 0) && (user.books.first.tweets.count != 0))
+          @client = Twitter::REST::Client.new do |config|
+            config.consumer_key        = ENV["TWITTER_API_KEY"] 
+            config.consumer_secret     = ENV["TWITTER_API_SECRET"] 
+            # Change this in prod
+            config.access_token        = '4757590114-Kok6YXQBEENVeCKJv3WScIaOjNgS599fHPDoCoS'
+            config.access_token_secret = 'gi8mWVzBJWCkElZm7vwR6423iXbwID2rLDmRklPQupgC0'
+          end
+
+
+          if user.has_reached_max_tweets?
+            user.reset_tweets_index
+          end
+
+          handle = user.handle
+          tweet = user.books.first.tweets[user.tweets_index].body
+          user.increase_tweets_index
+          @client.update(handle + " " + tweet)  
+        end    
+      end
+  end
 end
