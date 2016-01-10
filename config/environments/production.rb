3037cd15e176464086b86e9e3f9fef51d976d25d
Gabriel Stein <g@gabestein.com>
2016-01-10 11:48:09 -0500

    Try precompiling in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w( resque_web/lifebuoy.png )
+
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
