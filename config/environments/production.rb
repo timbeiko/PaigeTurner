8fc66fdfa1d065b7847a8e17301980665bbb7b91
Gabriel Stein <g@gabestein.com>
2016-01-10 11:52:00 -0500

    Change assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.precompile += %w( resque_web/lifebuoy.png )
-
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
