517dfc61ad29125fa53109ac0d6f8d789a37c647
tbeiko <t.beiko23@gmail.com>
2016-01-09 21:11:46 -0500

    customize tweets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110004129) do
+ActiveRecord::Schema.define(version: 20160110011044) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
